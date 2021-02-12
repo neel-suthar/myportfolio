b8ce68fdd05eddbbd77f77c8fabc9b8a2b3884f8
Neel Suthar <47631351+neel-suthar@users.noreply.github.com>
2021-02-11 19:36:34 -0600

    added projects and live link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
+import { faWindowClose, faDesktop } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Projects() {
@@ -15,33 +15,36 @@ function Projects() {
   const projects = [
     {
       id: 0,
-      title: "Rating Predictor on Game Review",
-      technologies: "(Python, NumPy, Pandas, SVM, sklearn, Python Flask, AWS EC2)",
-      description: `◉ Developed a Flask app to predict the rating of a given review on a scale of 1-10 and deployed it on AWS EC2 instance.
-      ◉ Developed a function to clean and format the reviews and ratings from a BoardGameGeek Reviews(13m) dataset.
-      ◉ Vectorized reviews using sklearn TfidfVectorizer for model training and normalized their ratings to gain better accuracy.
-      ◉ Trained the data using sklearn LinearSVC then saved the model as well as vectorizer using pickle to use for prediction.`,
-      gihubLink: "https://github.com/neel-suthar"
-    },
-    {
-      id: 1,
       title: "Movie Recommendation System",
       technologies: "(Python, Pandas, NumPy, The Movies Dataset)",
       description: `◉ Built a system to recommend several movies to a user based on their input movie with help of The Movies Dataset.
       ◉ Developed a logic to identify and convert unique genres, cast, and crew information into binary format for each movie.
       ◉ Built a distance function to calculate the similarity scores between two movies using genres and cast information.
       ◉ Developed kNN from scratch using distance function to get neighbors based on similarity; got highly accurate results.`,
-      gihubLink: "https://github.com/neel-suthar/movie_recomm_system"
+      gihubLink: "https://github.com/neel-suthar/movie_recomm_system",
+      live: null
     },
     {
-      id: 2,
+      id: 1,
       title: "Object Counting on webcam – Object Detection",
       technologies: "(Python, keras, TensorFlow, OpenCV, pandas, YOLOv3)",
       description: `◉ Trained object detection model with the help of MS COCO dataset using YOLOv3 object detection system and Keras.
       ◉ Created image processing steps to prepare an image for prediction using Keras and used Matplotlib to display output.
       ◉ Built a logic to process the live webcam feed for prediction, identifying objects, and bounding boxes in a live frame.
       ◉ Made detection process easy by using classes array to identify specific objects in a frame; can be tweaked easily.`,
-      gihubLink: "https://github.com/neel-suthar/object_detection_CSE6363"
+      gihubLink: "https://github.com/neel-suthar/object_detection_CSE6363",
+      live: null
+    },
+    {
+      id: 2,
+      title: "Rating Predictor on Game Review",
+      technologies: "(Python, NumPy, Pandas, SVM, sklearn, Python Flask, AWS EC2)",
+      description: `◉ Developed a Flask app to predict the rating of a given review on a scale of 1-10 and deployed it on AWS EC2 instance.
+      ◉ Developed a function to clean and format the reviews and ratings from a BoardGameGeek Reviews(13m) dataset.
+      ◉ Vectorized reviews using sklearn TfidfVectorizer for model training and normalized their ratings to gain better accuracy.
+      ◉ Trained the data using sklearn LinearSVC then saved the model as well as vectorizer using pickle to use for prediction.`,
+      gihubLink: "https://github.com/neel-suthar",
+      live: null
     },
     {
       id: 3,
@@ -49,7 +52,25 @@ function Projects() {
       technologies: "(Python)",
       description: `◉ Built kNN classifier from scratch with different distance measures and custom data cleaning, preprocessing functions.
       ◉ Applied kNN on breast_cancer, car, and hayes_roth dataset and was able to achieve higher accuracy than WEKA.`,
-      gihubLink: "https://github.com/neel-suthar/kNN_classifier"
+      gihubLink: "https://github.com/neel-suthar/kNN_classifier",
+      live: null
+    },
+    {
+      id: 4,
+      title: "Covid Cases",
+      technologies: "(Java, Spring, Thymleaf, Bulma, HTML)",
+      description: `◉ Developed a web app using Java & Spring Boot which shows confirmed covid cases as of today and changes in cases.
+      ◉ Used Scheduling to fetch the new data every day automatically from the source and deployed the app on Heroku.`,
+      gihubLink: "https://github.com/neel-suthar/covid19_cases_global",
+      live: "https://covid19-global-daily.herokuapp.com/"
+    },
+    {
+      id: 5,
+      title: "Daily Stars-APOD",
+      technologies: "(React, JavaScript, HTML, CSS)",
+      description: `◉ Built a web app that display astronomy picture of a day using NASA-apod API, deployed it using Heroku.`,
+      gihubLink: "https://github.com/neel-suthar/daily_stars",
+      live: "https://daily-stars-apod.herokuapp.com/"
     }
   ];
 
@@ -70,7 +91,8 @@ function Projects() {
             <p className="modal__title">{projects[projectId].title}</p>
             <p className="modal__subtitle">{projects[projectId].technologies}</p>
             <p className="modal__text" style={{ whiteSpace: "pre-line" }}>{projects[projectId].description}</p>
-            <a href={projects[projectId].gihubLink} target="_blank" rel="noreferrer" className="modal-btn" onClick={handleClose} style={{ textAlign: "center" }}>View Project &nbsp;<FontAwesomeIcon icon={faGithub} /></a>
+            <a href={projects[projectId].gihubLink} target="_blank" rel="noreferrer" className="modal-btn" onClick={handleClose} style={{ textAlign: "center" }}>View Code &nbsp;<FontAwesomeIcon icon={faGithub} /></a>
+            { projects[projectId].live === null ? '' : (<a href={projects[projectId].live} target="_blank" rel="noreferrer" className="modal-btn" onClick={handleClose} style={{ textAlign: "center", backgroundColor: "red" }}>Watch it Live! &nbsp;<FontAwesomeIcon icon={faDesktop} /></a>) }
           </div>
         </div>
       </div>
